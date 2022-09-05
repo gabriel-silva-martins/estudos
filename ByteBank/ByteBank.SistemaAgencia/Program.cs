1b8a4b2553bb2cf258e316076aee5639b4dbc4e6
Gabriel Martins <Gabriel.Martins@simco.local>
2022-09-05 11:13:56 -0300

    mais alguns materiais do estudo no curso da alura

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ByteBank.SistemaAgencia;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 4885);
+
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
+        }
+    }
+}
